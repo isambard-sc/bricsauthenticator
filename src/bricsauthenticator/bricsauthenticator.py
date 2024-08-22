e8aa1bb37f7b844648da583c7a154cd7419b37b9
James C. Womack <J.C.Womack@bristol.ac.uk>
2024-08-22 14:59:10 +0000

    Fix issues with JWT validation
    * Validate signature and several registered claims
    * Verify presence of short_name and projects claims
    
    Also:
    
    * Remove dependency on requests (using tornado.httpclient instead)
    * In debug mode, log decoded JWT

--- src/bricsauthenticator/bricsauthenticator.py
+++ src/bricsauthenticator/bricsauthenticator.py
@@ -5,10 +5,10 @@ JupyterHub `Authenticator` for the BriCS JupyterHub service
 import json
 
 import jwt
-import requests
 from jupyterhub.auth import Authenticator
 from jupyterhub.handlers import BaseHandler
 from tornado import web
+from tornado.httpclient import AsyncHTTPClient
 
 
 class BricsLoginHandler(BaseHandler):
@@ -19,24 +19,43 @@ class BricsLoginHandler(BaseHandler):
             raise web.HTTPError(401, "Missing X-Auth-Id-Token header")
         
         oidc_server = "https://keycloak.isambard.ac.uk/realms/isambard"  # Hard-code to ensure it's a place we trust
-        oidc_config = requests.get(f"https://{oidc_server}/.well-known/openid-configuration").json()
+
+        http_client = AsyncHTTPClient()
+        try:
+            response = await http_client.fetch(
+                request=f"{oidc_server}/.well-known/openid-configuration",
+                method="GET"
+            )
+        except Exception as e:
+            self.log.exception(f"Encountered exception when fetching OIDC server config")
+            raise web.HTTPError(500) from e
+
+        oidc_config = json.loads(response.body)
         signing_algos = oidc_config["id_token_signing_alg_values_supported"]
 
-        jwks_client = jwt.PyJWKClient(oidc_config["jwks_uri"])
+        # A 'User-Agent' header is required, otherwise the oidc_server returns HTTP 403 forbidden
+        jwks_client = jwt.PyJWKClient(oidc_config["jwks_uri"], headers={'User-Agent' : f'PyJWT/{jwt.__version__}'})
         signing_key = jwks_client.get_signing_key_from_jwt(token)
 
-        # Verify signature of JWT token
         try:
-            # Decode the JWT token without verifying the signature
+            # Decode the JWT token and verify
+            # Verifies signature and values of audience (aud), issuer (iss),
+            # expiry (exp), and issued at (iat) claims
+            # Verifies presence of short_name and projects claims (used in JupyterHub)
             decoded_token = jwt.decode(
                 token, 
                 key=signing_key.key,
                 algorithms=signing_algos,
+                options={
+                    "verify_signature": True,
+                    "require": ["aud", "exp", "iss", "iat", "short_name", "projects"],
+                },
+                audience="zenith-jupyter",
+                issuer=oidc_server,
             )
-            # Print all key-value pairs in the JWT token (optional for debugging)
-            print("Decoded JWT Token:")
-            for key, value in decoded_token.items():
-                print(f"{key}: {value}")
+
+            # Log all key-value pairs in the JWT token (optional for debugging)
+            self.log.debug("Decoded JWT Token:\n" + "\n".join(f"{key}: {value}" for key, value in decoded_token.items()))
 
             # Extract the username (or other unique identifier) from the token
             username = decoded_token.get("short_name")
@@ -49,7 +68,6 @@ class BricsLoginHandler(BaseHandler):
             #  by inspecting the resources allocated to each project
 
             # Authenticate the user with JupyterHub
-            # user = self.user_from_username(username)
             user = await self.auth_to_user({"name": username, "auth_state": projects})
             self.set_login_cookie(user)
             next_url = self.get_next_url(user)
